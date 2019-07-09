1af21734eb38d4a2e58ac750b6ee62922f0218cb
Valerie Trujillo <valerietrujillo2@aol.com>
2019-07-09 14:46:35 -0700

    Updates

---
+++ precache-manifest.2e3b9b41fd39de4beddd0ad0f9dcbccf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "02869910549d422a2b1d",
+    "url": "/my-project/static/js/runtime~main.8ce5ffd9.js"
+  },
+  {
+    "revision": "a3d33efdd2d503682484",
+    "url": "/my-project/static/js/main.6e30aae4.chunk.js"
+  },
+  {
+    "revision": "f7142569cf865fa08926",
+    "url": "/my-project/static/js/2.208f9d9d.chunk.js"
+  },
+  {
+    "revision": "a3d33efdd2d503682484",
+    "url": "/my-project/static/css/main.85e2ba29.chunk.css"
+  },
+  {
+    "revision": "f7142569cf865fa08926",
+    "url": "/my-project/static/css/2.b6cddaab.chunk.css"
+  },
+  {
+    "revision": "5c16a63b0c3f07e776757cfac09eb9fc",
+    "url": "/my-project/index.html"
+  }
+];
